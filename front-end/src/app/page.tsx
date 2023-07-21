06d56af0fe573c3c132ae36b4813f5cb452a68c1
Ameya-OP <ameya.mjoshi481@gmail.com>
2023-07-21 12:22:49 +0530

    Created next js app.

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,18 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.card}>
+        <div className={styles.gridcontainer}>
+          <div className={styles.griditem}>
+
+          </div>
+          <div className={styles.griditem}>
+            
+          </div>
+        </div>
+      </div>      
+    </main>
+  )
+}
