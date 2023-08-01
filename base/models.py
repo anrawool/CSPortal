44c822902a47db4e4f24045874aa0895a703cbd7
anrawool <anrawool@gmail.com>
2023-08-01 18:01:36 +0530

    Custom User Model Base Established

--- base/models.py
+++ base/models.py
@@ -1,8 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from .manager import *
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class User(AbstractUser):
+
+    username = models.CharField(max_length=200, unique=True, default="Guest", null=True)
+    email = models.EmailField(max_length=200, unique=True, editable=True)
+    phone_number = models.IntegerField(null=True, editable=True)
+    profile_pic = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to="./uploads/profile_pics",
+        default="./uploads/profile_pics/default_male_avatar.svg",
+    )
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "phone_number"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+
 
 class HomeWorkPost(models.Model):
-    assigned_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="assigned_by", null=True)
+    assigned_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="assigned_by", null=True
+    )
     assigned_to = models.ManyToManyField(User, related_name="assigned_to")
     title = models.CharField(max_length=200)
     subject = models.CharField(max_length=200, default="General")
@@ -11,17 +40,19 @@ class HomeWorkPost(models.Model):
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
-        ordering = ['-created_at', '-updated_at']
+        ordering = ["-created_at", "-updated_at"]
 
 
 class NotificationPost(models.Model):
-    assigned_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='given_by', null=True)
-    assigned_to = models.ManyToManyField(User, related_name='give_to')
+    assigned_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="given_by", null=True
+    )
+    assigned_to = models.ManyToManyField(User, related_name="give_to")
     title = models.CharField(max_length=200)
     text = models.TextField()
-    related_file = models.FileField(upload_to='../media/uploads', null=True, blank=True)
+    related_file = models.FileField(upload_to="../media/uploads", null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_at', '-updated_at']
+        ordering = ["-created_at", "-updated_at"]
